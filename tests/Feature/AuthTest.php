1dda765d38ecda747badbfd3c4af870b62aa1f62
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-01-15 15:24:41 +0200

    [Add] Auth Test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -10,18 +10,68 @@ use Tests\TestCase;
 class AuthTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * user login test case
+     * create user then post a request to login
+     * check the token and response status
      */
-    public function test_example(): void
+    public function test_user_login(): void
     {
         $user = User::factory()->create([
-            'name' => 'TestUser'
+            'password' => bcrypt('password'),
         ]);
 
-        $this->actingAs($user);
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
 
-        $response = $this->get('/');
+        $response->assertStatus(200)->assertJsonStructure(['token']);
 
-        $response->assertStatus(200);
+        $this->assertAuthenticated();
+
+        $this->assertNotNull($user->tokens->first());
+    }
+
+    /**
+     * user register test case
+     */
+    public function test_user_can_register()
+    {
+        $user = User::factory()->make([
+            'password' => bcrypt('123456789'),
+        ]);
+
+        $postUserData = $user->toArray();
+
+        $postUserData['password'] = '123456789';
+        $postUserData['password_confirmation'] = '123456789';
+
+        $response = $this->postJson('/api/auth/register', $postUserData);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => 'User created successfully',
+            ])
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'phone',
+                    'type',
+                    'created_at',
+                    'updated_at',
+                ],
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'phone' => $user->phone,
+            'type' => $user->type
+        ]);
     }
 }
